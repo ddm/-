17f89efce0043430803e34c6777d6f4ec0ba439b
Dimitri del Marmol <dimitri.delmarmol@gmail.com>
2012-08-21 21:49:18 +0200

    ;

--- µ.js
+++ µ.js
@@ -2,51 +2,51 @@
 function µ(decorated) {
   return _.isFunction(decorated) ? decorated : _.extend(Object.create(decorated), {
     match: function(selector) {
-      var self = this;
-      var methods = _.difference(_.functions(self), _.functions(µ(null)));
+      var self = this
+      var methods = _.difference(_.functions(self), _.functions(µ(null)))
       var filter =
-        _.isString(selector) ? function(method) { return selector === method; }
-        : _.isRegExp(selector) ? function(method) { return selector.test(method); }
+        _.isString(selector) ? function(method) { return selector === method }
+        : _.isRegExp(selector) ? function(method) { return selector.test(method) }
         : _.isFunction(selector) ? selector
-        : false;
+        : false
       return methods.filter(filter).filter(function(method) {
-        return self[method].apply;
-      });
+        return self[method].apply
+      })
     },
     before: function(selector, decorator) {
-      var self = this;
-      var selected = self.match(selector);
+      var self = this
+      var selected = self.match(selector)
       _.each(selected, function(method) {
-        var original = self[method];
+        var original = self[method]
         self[method] = function() {
-          var new_arguments = decorator.apply(self, arguments);
-          return original.apply(self, new_arguments || arguments);
-        };
-      });
-      return self;
+          var new_arguments = decorator.apply(self, arguments)
+          return original.apply(self, new_arguments || arguments)
+        }
+      })
+      return self
     },
     after: function(selector, decorator) {
-      var self = this;
-      var selected = self.match(selector);
+      var self = this
+      var selected = self.match(selector)
       _.each(selected, function(method) {
-        var original = self[method];
+        var original = self[method]
         self[method] = function() {
-          var original_result = original.apply(self, arguments);
-          return decorator.apply(self, [ original_result ]) || original_result;
-        };
-      });
-      return self;
+          var original_result = original.apply(self, arguments)
+          return decorator.apply(self, [ original_result ]) || original_result
+        }
+      })
+      return self
     },
     insteadOf: function(selector, decorator) {
-      var self = this;
-      var selected = self.match(selector);
+      var self = this
+      var selected = self.match(selector)
       _.each(selected, function(method) {
         self[method] = function() {
-          return decorator.apply(self, arguments);
-        };
-      });
-      return self;
+          return decorator.apply(self, arguments)
+        }
+      })
+      return self
     }
-  });
+  })
 }
 /* vim: set ts=2 sw=2 noai et : */
